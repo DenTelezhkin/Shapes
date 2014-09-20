d9145983082bfcb41df1cf6367bd25ce03c2cb8e
Denys Telezhkin <strangervir@gmail.com>
2014-09-20 13:42:32 +0300

    add common header for ShapeKit.

---
+++ ShapeKit/ShapeKit.h
@@ -0,0 +1,16 @@
+//
+//  ShapeKit.h
+//  ShapeKitExample
+//
+//  Created by Denys Telezhkin on 20.09.14.
+//  Copyright (c) 2014 Denys Telezhkin. All rights reserved.
+//
+
+#import "DTShapeView.h"
+#import "DTShapeButton.h"
+
+#import "DTDimmingView.h"
+#import "DTProgressView.h"
+#import "DTAnimatableShapeLayer.h"
+
+#import "CATransaction+AnimateWithDuration.h"

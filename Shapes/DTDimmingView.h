d9ea3f37b6dcf49994f4b83e6da3e9744587f6f3
Taylan Kammer <taylan.kammer@gal-digital.de>
2015-05-08 14:22:17 +0200

    Fix imports.

--- Shapes/DTDimmingView.h
+++ Shapes/DTDimmingView.h
@@ -23,6 +23,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#import <UIKit/UIKit.h>
+
 #if __has_feature(nullability) // Xcode 6.3+
 #pragma clang assume_nonnull begin
 #else
--- Shapes/DTGraphicsConverter.h
+++ Shapes/DTGraphicsConverter.h
@@ -23,7 +23,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #if __has_feature(nullability) // Xcode 6.3+
 #pragma clang assume_nonnull begin
